1499fe410a42cd0f2cc979bc0fa13bf0aac55821
profgstv <gus.migueljorge@gmail.com>
2026-03-24 13:59:03 -0300

    Movimentação do Player com Animações

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,35 @@
 extends CharacterBody2D
 
+@onready var sprite: AnimatedSprite2D = $Sprite
+
+var sprite_direction := "down"
+
 const SPEED = 300.0
 
-func _physics_process(_delta):
+func animation(mov, dir):
+	sprite_direction = dir
+	sprite.play(mov + "_" + dir)
+	pass
+
+func _physics_process(_delta: float) -> void:
+	#movimento
 	var direction := Vector2(
-		Input.get_axis("ui_left", "ui_right"), 
-		Input.get_axis("ui_up", "ui_down") 
-		)
-	velocity = direction * SPEED
+		Input.get_axis("ui_left", "ui_right"),
+		Input.get_axis("ui_up", "ui_down")
+	)
+	velocity = direction.normalized() * SPEED
 
 	move_and_slide()
+	
+	#animação
+	if velocity.x > 0:
+		animation("run", "right")
+	elif velocity.x < 0:
+		animation("run", "left")
+	elif velocity.y > 0:
+		animation("run", "down")
+	elif velocity.y < 0:
+		animation("run", "up")
+	else:
+		animation("idle", sprite_direction)
+		pass
