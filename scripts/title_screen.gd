e48aeb49688de7aff2ac2c422696e40fc205a65f
profgstv <gus.migueljorge@gmail.com>
2026-04-02 13:59:06 -0300

    Aula Level Design

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -13,4 +13,8 @@ func _process(_delta):
 
 func _on_button_pressed():
 	get_tree().quit()
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level01.tscn")
 	pass # Replace with function body.
