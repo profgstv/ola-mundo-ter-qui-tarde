f971394c169177c0a6891346d42834f738f4ca84
profgstv <gus.migueljorge@gmail.com>
2026-05-07 13:58:53 -0300

    Programação da cena de Diálogo

--- scripts/dialogo.gd
+++ scripts/dialogo.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+
+var mostrar_mensagem: bool = false
 var titulo: String = "Olá Mundo!"
 var mensagem: String = "Estamos na Fabrica de Cultura."
 var nome_botoes: Array[String] = ["Ok", "Tá", "Blz"]
@@ -15,7 +17,20 @@ var func_botoes: Array[Callable] = [
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
-	pass
+	titulo_label.text = titulo
+	mensagem_label.text = mensagem
+	for index in nome_botoes.size():
+		h_box_container.add_child(Button.new())
+		h_box_container.get_child(index).text = nome_botoes[index]
+		h_box_container.get_child(index).size_flags_horizontal = 3
+		pass
 
 func _process(delta: float) -> void:
-	pass
+	if mostrar_mensagem and mensagem_label.visible_ratio < 1:
+		mensagem_label.visible_characters += int(100 * delta)
+		pass
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "start":
+		mostrar_mensagem = true
+		pass
